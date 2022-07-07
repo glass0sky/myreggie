dd474d466c6f1d2ab1f2483713044dca0296150f
2451536149@qq.com <2451536149@qq.com>
2022-07-07 15:24:44 +0800

    刚开始创建项目...

---
+++ src/main/java/com/mdy/myreggie/MyReggieTakeOutApp.java
@@ -0,0 +1,18 @@
+package com.mdy.myreggie;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.ApplicationContext;
+
+@Slf4j
+@SpringBootApplication
+@ServletComponentScan
+public class MyReggieTakeOutApp {
+    public static void main(String[] args) {
+        ApplicationContext context = SpringApplication.run(MyReggieTakeOutApp.class, args);
+        log.info("项目启动成功.....");
+    }
+}
